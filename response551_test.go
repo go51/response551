2b32f805ef7d90773b9c3b43c39c02fe84140e48
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-19 12:16:49 +0900

    refs #2 エラーレスポンスの構造体を定義しそれを返すメソッドを実装する

--- response551_test.go
+++ response551_test.go
@@ -1,6 +1,7 @@
 package response551_test
 
 import (
+	"fmt"
 	"github.com/go51/response551"
 	"net/http"
 	"testing"
@@ -52,5 +53,40 @@ func TestRedirect(t *testing.T) {
 	}
 	if ret303.Uri() != uri {
 		t.Error("リダイレクトレスポンス構造体の生成に失敗しました。")
+	}
+}
+
+func TestError(t *testing.T) {
+	ret403 := response551.Error(403, "アクセスするには会員登録をおこないログインする必要があります。")
+	ret404 := response551.Error(404, "指定されたカテゴリーは存在しません。")
+
+	if ret403.Code() != 403 {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+	if ret403.Text() != http.StatusText(403) {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+	if ret403.Message() != "アクセスするには会員登録をおこないログインする必要があります。" {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+	if ret403.String() != "アクセスするには会員登録をおこないログインする必要があります。" {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+
+	if ret404.Code() != 404 {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+	if ret404.Text() != http.StatusText(404) {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+	if ret404.Message() != "指定されたカテゴリーは存在しません。" {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+	if ret404.String() != "指定されたカテゴリーは存在しません。" {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
+	}
+
+	if fmt.Sprint(ret404) != "指定されたカテゴリーは存在しません。" {
+		t.Error("エラーレスポンス構造体の生成に失敗しました。")
 	}
 }
